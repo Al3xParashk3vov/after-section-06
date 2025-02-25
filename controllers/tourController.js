e484d2c1f9c069f49f479133616c3c19f5c415f1
Alexander Parashkevov <122312028+Al3xParashk3vov@users.noreply.github.com>
2025-02-25 07:00:25 +0200

    Almost there

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,31 +1,19 @@
 const Tour = require('./../models/tourModel')
+const APIFeatures = require('./../utils/apiFeatures')
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next()
+}
+
+
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Build query
-    // 1A) Filtering
-    const queryObj = {...req.query}
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el])
-  
-    // 1B) Advanced filtering
-    let queryStr = JSON.stringify(queryObj)
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-    let query = Tour.find(JSON.parse(queryStr))
-
-    // 2) Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ')
-      query = query.sort(sortBy)
-    } else {
-      query = query.sort('-createdAt')
-    }
-
     // Execute query
-    const tours = await query
-
-    // const query = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy')
-
+    const features = new APIFeatures(Tour.find(), req.query).filter().sort().limitFields().paginate();
+    const tours = await features.query
 
     // Send response
     res.status(200).json({
